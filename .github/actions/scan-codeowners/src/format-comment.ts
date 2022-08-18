5f7054b21a35c943351ceb9592c22faf941cea96
Cory Forsyth <cory.forsyth@addepar.com>
2022-08-18 14:26:14 +0200

    other ts fix

--- .github/actions/scan-codeowners/src/format-comment.ts
+++ .github/actions/scan-codeowners/src/format-comment.ts
@@ -1,7 +1,7 @@
 import * as handlebars from 'handlebars'
 import * as fs from 'fs'
 import * as path from 'path'
-import {UUID} from './UUID'
+import {UUID} from './utils/constants'
 
 const templateFile = fs.readFileSync(
   path.join(__dirname, 'templates', 'summary.hbs'),
