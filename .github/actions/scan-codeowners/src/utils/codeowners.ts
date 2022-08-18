f95bcebbbaae6b3fc938723d0253b541eeea567d
Cory Forsyth <cory.forsyth@addepar.com>
2022-08-18 15:26:19 +0200

    handle missing codeowners files

--- .github/actions/scan-codeowners/src/utils/codeowners.ts
+++ .github/actions/scan-codeowners/src/utils/codeowners.ts
@@ -1,6 +1,7 @@
 import ignore from 'ignore'
 import {MyOctokit} from './types'
 import debugBase from './debug'
+import {RequestError} from '@octokit/request-error'
 
 const debug = debugBase.extend('codeowners-file')
 
@@ -27,26 +28,50 @@ function parseCodeowners(codeowners: string): [string, string][] {
     })
 }
 
-// TODO, catch (?) the 404 if the file doesn't exist
-// TODO: try multiple file locations as specified by github's docs
-// TODO: coalesce unowned files to shared paths. Ie if /path/to/a.txt and /path/to/b.txt, can just print `/path/to/*` had unowned files
 export async function fetchCodeownersPatterns(
   octokit: MyOctokit,
   {owner, repo, ref}
 ): Promise<string[]> {
-  const codeowners = await fetchFile(octokit, {
-    owner,
-    repo,
-    ref,
-    path: 'CODEOWNERS'
-  })
-  const patterns = parseCodeownersPatterns(codeowners)
+  const codeownersFiles = await findCodeownersFiles(octokit, {owner, repo, ref})
+  let patterns: string[] = []
+  for (const [file, contents] of Object.entries(codeownersFiles)) {
+    const parsedPatterns = parseCodeownersPatterns(contents)
+    debug(`Parsed %o patterns from file %o`, parsedPatterns.length, file)
+    patterns = [...patterns, ...parsedPatterns]
+  }
   debug(
     `Found %o codeowners patterns. first 100 %O`,
     patterns.length,
     patterns.slice(0, 100)
   )
   return patterns
+}
+
+async function findCodeownersFiles(
+  octokit: MyOctokit,
+  {owner, repo, ref}
+): Promise<Record<string, string>> {
+  const paths = ['CODEOWNERS', '.github/CODEOWNERS']
+
+  const results: Record<string, string> = {}
+
+  for (const path of paths) {
+    try {
+      results[path] = await fetchFile(octokit, {owner, repo, ref, path})
+    } catch (e) {
+      if (e instanceof RequestError) {
+        if (e.status === 404) {
+          debug(`received 404 response when fetching file at path %o`, path)
+        } else {
+          throw e
+        }
+      } else {
+        throw e
+      }
+    }
+  }
+
+  return results
 }
 
 async function fetchFile(
@@ -67,7 +92,7 @@ async function fetchFile(
     const decoded = encoded.toString('utf8')
     return decoded
   } else {
-    debug(`file was empty or missing at path %o`, path)
+    debug(`unexpectedly found no content for file at path %o`, path)
     return ''
   }
 }
